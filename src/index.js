c6748332e588f8a6ebaa661636d85abfb1c1b576
Vicker <vcalero2001@gmail.com>
2025-06-29 02:53:53 +0200

    Created new files for the generation of nav pages
    Created and added the function for the creation of
    home, menu and about pages. These are only the
    bones.
    Also, exported them into the index.js file.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import "./styles.css";
+import homePageLoad from "./pages/home";
+import menuPageLoad from "./pages/menu";
+import aboutPageLoad from "./pages/about";
 
 console.log("Welcome to this amazing restaurant");
