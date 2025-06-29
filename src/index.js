4f97050e06b73273679dd15079e7058b6ed0e147
Vicker <vcalero2001@gmail.com>
2025-06-30 01:27:07 +0200

    Nav buttons have id and envet listeners
    Now everytime the buttons are pressed, the content
    div is cleaned and the new content is added.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./styles.css";
 import homePageLoad from "./pages/home";
 import menuPageLoad from "./pages/menu";
 import aboutPageLoad from "./pages/about";
+import home from "./pages/home";
 
 console.log("Welcome to this amazing restaurant");
 
@@ -11,4 +12,28 @@ function clearContent() {
     content.innerHTML = "";
 }
 
-content.appendChild(homePageLoad());
+//=============
+// Initial load
+//=============
+content.appendChild(aboutPageLoad());
+
+//=============
+// NavBar eventListeners
+//=============
+const homeButton = document.getElementById("home");
+homeButton.addEventListener("click", () => {
+    clearContent();
+    content.append(homePageLoad());
+})
+
+const menuButton = document.getElementById("menu");
+menuButton.addEventListener("click", () => {
+    clearContent();
+    content.append(menuPageLoad());
+})
+
+const aboutButton = document.getElementById("about");
+aboutButton.addEventListener("click", () => {
+    clearContent();
+    content.append(aboutPageLoad());
+})
